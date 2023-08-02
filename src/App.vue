6617beb33d7acbf12123be1f43f534a453647044
Aribaba <86304459+Gidi-on@users.noreply.github.com>
2023-08-02 01:04:20 +0100

    config

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="font-montserrat overflow-x-hidden">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Dashboard from "./views/Dash.vue";
+export default {
+  name: "App",
+  components: {
+    Dashboard,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
+}
+</style>
